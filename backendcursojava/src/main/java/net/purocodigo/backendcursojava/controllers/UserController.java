e0e65efe1d774dfd41b01e63c7821cd0da5c07db
BernardoModerno <bernardo.moderno@gmail.com>
2023-01-04 16:18:00 -0300

    Post e Get de teste

---
+++ backendcursojava/src/main/java/net/purocodigo/backendcursojava/controllers/UserController.java
@@ -0,0 +1,22 @@
+package net.purocodigo.backendcursojava.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping
+    public String getUser(){
+        return "get user detail";
+    }
+
+    @PostMapping
+    public String createUser() {
+        return "create new user";
+    }
+    
+}
